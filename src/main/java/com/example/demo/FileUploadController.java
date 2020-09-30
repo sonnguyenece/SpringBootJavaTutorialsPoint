f79954d67f23615588bcc7c54b91e5418473c412
nmson <nmson@ecpay.vn>
2020-09-30 14:18:13 +0700

    @Scheduler

---
+++ src/main/java/com/example/demo/FileUploadController.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+@RestController
+public class FileUploadController {
+    @RequestMapping(value = "/upload", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String fileUpload(@RequestParam("file") MultipartFile file) throws IOException {
+        File convertFile = new File("/var/tmp" + file.getOriginalFilename());
+        convertFile.createNewFile();
+        FileOutputStream fileOutputStream = new FileOutputStream(convertFile);
+        fileOutputStream.write(file.getBytes());
+        fileOutputStream.close();
+        return "File is upload successfully";
+    }
+}
